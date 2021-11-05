e57d6fc5fee429b975515d8b77cc28e75672e0ea
Simone-De-Santis <simone.agentelazio@yahoo.it>
2021-11-05 15:30:13 +0100

    // Creation of 'ingredient' table with relevant data

---
+++ database/migrations/2021_11_05_142837_create_ingredients_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIngredientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ingredients', function (Blueprint $table) {
+            // Creation of 'categories' table with relevant data
+            $table->id();
+            $table->string('name', 100);
+            $table->string('url')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ingredients');
+    }
+}
