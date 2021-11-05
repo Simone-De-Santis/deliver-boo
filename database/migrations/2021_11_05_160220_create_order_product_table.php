394263d7358e82d05191a87226fd703d00d00014
Simone-De-Santis <simone.agentelazio@yahoo.it>
2021-11-05 17:31:18 +0100

    finisched relation table db

--- database/migrations/2021_11_05_160220_create_order_product_table.php
+++ database/migrations/2021_11_05_160220_create_order_product_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateIngredientProductTable extends Migration
+class CreateOrderProductTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,12 @@ class CreateIngredientProductTable extends Migration
      */
     public function up()
     {
-        Schema::create('ingredient_product', function (Blueprint $table) {
-
+        Schema::create('order_product', function (Blueprint $table) {
             $table->id();
             // Foreign key link to 'product_id'
             $table->foreignId('product_id')->constrained()->onDelete('cascade');
-            // Foreign key link to 'ingredient_id'
-            $table->foreignId('ingredient_id')->constrained()->onDelete('cascade');
-
+            // Foreign key link to 'order_id'
+            $table->foreignId('order_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -32,6 +30,6 @@ class CreateIngredientProductTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('ingredient_product');
+        Schema::dropIfExists('order_product');
     }
 }
