76a70fc4ad7cf710f760c0e9207c6a8b4d1dac0f
Giorgio Andrei <cipuandrei@libero.it>
2021-11-06 14:11:09 +0100

    foreign ids filled in products table

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Arr;
 use App\Models\Product;
+use App\User;
+use App\Models\Type;
 use Faker\Generator as Faker;
 
 class ProductsTableSeeder extends Seeder
@@ -13,10 +16,18 @@ class ProductsTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
+
+        /* Selecting id column, then with pluck we get back the collection with the ids array inside, but we 
+        just need the array itself, so we get it with toArray() */
+        $types_id = Type::pluck('id')->toArray();
+        $user_id = User::pluck('id')->toArray();
+
         //Added 'for loop' to generate data within the 'products' table
         for ($i = 0; $i < 20; $i++) {
             $new_product = new Product();
 
+            $new_product->user_id = Arr::random($user_id);
+            $new_product->type_id = Arr::random($types_id);
             $new_product->name = $faker->word();
             $new_product->discount  = $faker->randomFloat(2, 0, 99);
             $new_product->price = $faker->randomFloat(2, 0, 99);
