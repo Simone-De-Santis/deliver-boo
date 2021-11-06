d80d691620a07d555163e3391c083dca53135b13
Giorgio Andrei <cipuandrei@libero.it>
2021-11-06 15:29:11 +0100

    products ids added to pivot table

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Arr;
 use App\Models\Product;
+use App\Models\Ingredient;
 use App\User;
 use App\Models\Type;
 use Faker\Generator as Faker;
@@ -20,13 +21,14 @@ class ProductsTableSeeder extends Seeder
         /* Selecting id column, then with pluck we get back the collection with the ids array inside, but we 
         just need the array itself, so we get it with toArray() */
         $types_id = Type::pluck('id')->toArray();
-        $user_id = User::pluck('id')->toArray();
+        $users_id = User::pluck('id')->toArray();
+        $ingredients_id = Ingredient::pluck('id')->toArray();
 
         //Added 'for loop' to generate data within the 'products' table
         for ($i = 0; $i < 20; $i++) {
             $new_product = new Product();
 
-            $new_product->user_id = Arr::random($user_id);
+            $new_product->user_id = Arr::random($users_id);
             $new_product->type_id = Arr::random($types_id);
             $new_product->name = $faker->word();
             $new_product->discount  = $faker->randomFloat(2, 0, 99);
@@ -36,6 +38,11 @@ class ProductsTableSeeder extends Seeder
             $new_product->url = $faker->imageUrl(640, 680, 'animals', true);
 
             $new_product->save();
+
+            for ($x = 0; $x < 10; $x++) {
+                /* Attaching ingredient id to pivot table */
+                $new_product->ingredients()->attach(Arr::random($ingredients_id));
+            }
         }
     }
 }
