353df0a57596896daf4fb46225d7cd93defb2fd1
MarilenaCantisani <marilena.cantisani97@gmail.com>
2021-11-10 12:55:19 +0100

    Added users home

--- resources/views/Admin/home.blade.php
+++ resources/views/Admin/home.blade.php
@@ -4,30 +4,11 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Dashboard') }}</div>
-
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-
-                        {{ __('You are logged in!') }}
-                    </div>
-                </div>
-            </div>
-            <h1>questa è la home admin </h1>
+            
+            <h1>Benvenuto {{ $user->name }} </h1>
         </div>
-        <h3 class="mt-5">Prodotti associati all'utente attuale:</h3>
-        <ul>
-            @foreach ($products as $product)
-                <li>Product id: {{ $product->id }}</li>
-                <li>Product name: {{ $product->name }}</li>
-                <br>
-            @endforeach
-        </ul>
+       <h2>
+           Statistiche di tutte cose
+       </h2>
     </div>
 @endsection
