d1c81d7a18e69f44b57d0129896f7ed5cccf0794
Giorgio Andrei <cipuandrei@libero.it>
2021-11-15 11:39:36 +0100

    form structure and script added

---
+++ resources/views/payment/index.blade.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+
+
+
+    <!-- Styles -->
+    <style>
+
+    </style>
+</head>
+
+<body>
+    <div class="flex-center position-ref full-height">
+        @if (Route::has('login'))
+            <div class="top-right links">
+                @auth
+                    <a href="{{ url('/home') }}">Home</a>
+                @else
+                    <a href="{{ route('login') }}">Login</a>
+
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}">Register</a>
+                    @endif
+                @endauth
+            </div>
+        @endif
+        {{-- I VARI FORM LI TROVI A QUESTO LINK:  https://developer.paypal.com/braintree/docs/guides/hosted-fields/examples/javascript/v3 --}}
+        <div class="wrapper">
+            <div class="checkout container">
+
+                <header>
+                    <h1>Hi, <br>Let's test a transaction</h1>
+                    <p>
+                        Make a test payment with Braintree using PayPal or a card
+                    </p>
+                </header>
+
+                <form method="post" id="payment-form" action="">
+                    <section>
+                        <label for="amount">
+                            <span class="input-label">Amount</span>
+                            <div class="input-wrapper amount-wrapper">
+                                <input id="amount" name="amount" type="tel" min="1" placeholder="Amount" value="10">
+                            </div>
+                        </label>
+
+                        <div class="bt-drop-in-wrapper">
+                            <div id="bt-dropin"></div>
+                        </div>
+                    </section>
+
+                    <input id="nonce" name="payment_method_nonce" type="hidden" />
+                    <button class="button" type="submit"><span>Test Transaction</span></button>
+                </form>
+            </div>
+        </div>
+    </div>
+    {{-- SCRIPT PAGAMENTI BRANITREE --}}
+    <script src="https://js.braintreegateway.com/web/dropin/1.32.0/js/dropin.min.js"></script>
+    <script>
+        var form = document.querySelector('#payment-form');
+        var client_token = "";
+
+        braintree.dropin.create({
+            authorization: client_token,
+            selector: '#bt-dropin',
+            paypal: {
+                flow: 'vault'
+            }
+        }, function(createErr, instance) {
+            if (createErr) {
+                console.log('Create Error', createErr);
+                return;
+            }
+            form.addEventListener('submit', function(event) {
+                event.preventDefault();
+
+                instance.requestPaymentMethod(function(err, payload) {
+                    if (err) {
+                        console.log('Request Payment Method Error', err);
+                        return;
+                    }
+
+                    // Add the nonce to the form and submit
+                    document.querySelector('#nonce').value = payload.nonce;
+                    form.submit();
+                });
+            });
+        });
+    </script>
+
+</body>
+
+</html>
