e9929c3fe8b8c001853c0db3a86667d5d98cecdd
Rabab-art <bouhamla.rabab@gmail.com>
2021-11-15 22:23:55 +0100

    style

--- resources/views/Guest/home.blade.php
+++ resources/views/Guest/home.blade.php
@@ -1,43 +1,77 @@
-{{-- Added general layout  --}}
-@extends('layouts.app');
+{{-- Added general layout --}}
+@extends('layouts.app')
 {{-- Added cdn of 'vue' and 'axios' --}}
 @section('cdns')
-  <script src='https://cdnjs.cloudflare.com/ajax/libs/axios/0.21.1/axios.js'
-    integrity='sha512-otOZr2EcknK9a5aa3BbMR9XOjYKtxxscwyRHN6zmdXuRfJ5uApkHB7cz1laWk2g8RKLzV9qv/fl3RPwfCuoxHQ=='
-    crossorigin='anonymous'></script>
-  <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/axios/0.21.1/axios.js'
+  integrity='sha512-otOZr2EcknK9a5aa3BbMR9XOjYKtxxscwyRHN6zmdXuRfJ5uApkHB7cz1laWk2g8RKLzV9qv/fl3RPwfCuoxHQ=='
+  crossorigin='anonymous'></script>
+<script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
 @endsection
 {{-- Content section with all restaurant --}}
 @section('content')
-  <div id="app">
-    <section class="container">
-      <nav class="navbar navbar-light bg-light">
-        <form class="form-inline">
-          <input v-model='search_bar' @keyup.enter="getRestaurants()" class="form-control mr-sm-2" type="search"
-            placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
-      </nav>
-      <div v-for="restaurant in restaurants">
-        @{{ restaurant . activity_name }}
+<div id="app">
+  <section class="container">
+    <nav class="">
+      <form class="form-inline mb-4">
+        <input v-model='search_bar' @keyup.enter="getRestaurants()" class="form-control mr-sm-2" type="search"
+          placeholder="Search" aria-label="Search">
+        <button class="btn btn-warning my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </nav>
+
+
+
+
+    <div class="carosel carousel slide" data-ride="carousel" id="carouselExampleIndicators">
+      <ul v-for="restaurant in restaurants" class=" carosel-two " style="transform:translateX(0px);">
+        <li class="slide  slide-two ">
+          <button class="short-cut-title" type="button">
+            <div class="" role="presentation">
+              <div class="ccl"></div>
+              <div class=""><span class="">Offerte</span></div>
+            </div>
+          </button>
+        </li>
+      </ul>
+      <a class="left carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        
+      </a>
+      <a class="right carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      
+      </a>
+    </div>
+
+
+   
+
+
+
+
+
+    <div class="row rest-row">
+      <div v-for="restaurant in restaurants" class="col-sm-4">
+        <h1>@{{ restaurant . activity_name }}</h1>
         <div class="card" style="width: 18rem;">
-          <img class="card-img-top" src="" alt="Card image cap">
+          <img class="card-img-top" src="/images/fresh.jpg" alt="Card image cap">
           <div class="card-body">
             <h5 class="card-title">@{{ restaurant . activity_name }}</h5>
             <p class="card-text">@{{ restaurant . description }}</p>
             <h1>id @{{ restaurant . id }}</h1>
-            <a :href="'/user/'+restaurant.id" class="btn btn-primary">Dettagli</a>
+            <a :href="'/user/'+restaurant.id" class="btn btn-warning">Dettagli</a>
           </div>
         </div>
-        <hr>
+
       </div>
-  </div>
-  </section>
+    </div>
+</div>
+</section>
 @endsection
 {{-- Added the script section for the Vue logic --}}
 @section('script-end')
-  <script>
-    Vue.config.devtools = true;
+<script>
+  Vue.config.devtools = true;
     // Initialized a new instance of 'vue'
     const app = new Vue({
       el: '#app',
@@ -57,5 +91,8 @@
         this.getRestaurants();
       },
     })
-  </script>
+
+
+    
+</script>
 @endsection
--- resources/views/Guest/show.blade.php
+++ resources/views/Guest/show.blade.php
@@ -1,73 +1,74 @@
 {{-- Added general layout --}}
-@extends('layouts.app');
+@extends('layouts.app')
 {{-- Added cdn of 'vue' and 'axios' --}}
 @section('cdns')
-    <script src='https://cdnjs.cloudflare.com/ajax/libs/axios/0.21.1/axios.js'
-        integrity='sha512-otOZr2EcknK9a5aa3BbMR9XOjYKtxxscwyRHN6zmdXuRfJ5uApkHB7cz1laWk2g8RKLzV9qv/fl3RPwfCuoxHQ=='
-        crossorigin='anonymous'></script>
-    <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/axios/0.21.1/axios.js'
+    integrity='sha512-otOZr2EcknK9a5aa3BbMR9XOjYKtxxscwyRHN6zmdXuRfJ5uApkHB7cz1laWk2g8RKLzV9qv/fl3RPwfCuoxHQ=='
+    crossorigin='anonymous'></script>
+<script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
 @endsection
 {{-- Content section with selected restaurant products --}}
 @section('content')
-    {{-- Id app to add link with Vue --}}
-    <div id="app">
-        <section class="container">
-            <h1>Prodotti del ristorante {{ $user->activity_name }} </h1>
-            <div id="products-section" class="row">
-                {{-- Products section --}}
-                <div class="col-6">
-                    {{-- Cycle foreach to print all the products of the restaurant --}}
+{{-- Id app to add link with Vue --}}
+<div id="app">
+    <section class="container bg-yellow">
+        <h1>Prodotti del ristorante {{ $user->activity_name }} </h1>
+        <div id="products-section" class="row">
+            {{-- Products section --}}
+            <div class="col d-flex">
+                <div class="">
+                    {{-- Cycle foreach to print all the products of the rsestaurant --}}
                     @foreach ($user->products as $product)
-                        <div class="card" style="width: 18rem;">
-                            <img class="card-img-top" src="{{ asset('storage/' . $product->url) }}"
-                                alt="immagine-prodotto">
-                            <div class="card-body">
-                                <h5 class="card-title">{{ $product->name }}</h5>
-                                <h5 class="card-title">{{ $product->id }}</h5>
-                                <p class="card-text">{{ $product->description }}</p>
-                                <p class="card-text">€ {{ $product->price }}</p>
-                                <a href="/" class="btn btn-primary">Indietro</a>
-                                <button class="btn btn-warning" v-on:click="addProduct({{ $product }})">Aggiungi al
-                                    carrello</button>
-                            </div>
+                    <div class="card mb-5" style="width: 30rem;">
+                        <img class="card-img-top" src="{{ asset('storage/' . $product->url) }}" alt="immagine-prodotto">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $product->name }}</h5>
+                            <h5 class="card-title">{{ $product->id }}</h5>
+                            <p class="card-text">{{ $product->description }}</p>
+                            <p class="card-text">€ {{ $product->price }}</p>
+                            <a href="/" class="btn btn-primary ">Indietro</a>
+                            <button class="btn btn-warning" v-on:click="addProduct({{ $product }})">Aggiungi al
+                                carrello</button>
                         </div>
-                        <hr>
+                    </div>
+
                     @endforeach
                 </div>
-                {{-- Cart section --}}
-                <div v-if="cart.length > 0">
-                    <div id="cart-section" class="col" v-if="cart[0].user_id == {{ $user->id }}">
-                        <h1>Il mio carrello:</h1>
-                        <div v-for="(item,index) in cart">
-                            <ul>
-                                <li>
-                                    @{{ item . name }}
-                                    <button class="btn btn-secondary" v-on:click="decreaseQuantity(item),setLocalStorage()">
-                                        <i class="fas fa-minus"></i>
-                                    </button>
-                                    @{{ item . quantity }}
-                                    <button class="btn btn-secondary" v-on:click="addQuantity(item),setLocalStorage()">
-                                        <i class="fas fa-plus"></i>
-                                    </button>
-                                    <button class="btn btn-danger"
-                                        v-on:click="removeProduct(index),setLocalStorage()">Rimuovi</button>
-                                </li>
-                            </ul>
-                        </div>
-                        <span><strong>Totale:</strong>@{{ totalCart }}</span>
-                        <button class="btn btn-success">
-                            <a class="text-white text-decoration-none" v-on:click="setLocalStorage()"
-                                href="{{ route('checkout.create') }}">Procedi con il pagamento</a>
-                        </button>
-                    </div>
-                </div>
-
             </div>
+            {{-- Cart section --}}
+            <div v-if="cart.length > 0" class="cart bg-white cart d-flex" >
+                <div id="cart-section" class="col  justify-content-center" style="width: 30rem;" v-if="cart[0].user_id == {{ $user->id }}">
+                    <h1>Il mio carrello:</h1>
+                    <div v-for="(item,index) in cart">
+                        <ul>
+                            <li>
+                                @{{ item . name }}
+                                <button class="btn btn-secondary" v-on:click="decreaseQuantity(item),setLocalStorage()">
+                                    <i class="fas fa-minus"></i>
+                                </button>
+                                @{{ item . quantity }}
+                                <button class="btn btn-secondary" v-on:click="addQuantity(item),setLocalStorage()">
+                                    <i class="fas fa-plus"></i>
+                                </button>
+                                <button class="btn btn-danger"
+                                    v-on:click="removeProduct(index),setLocalStorage()">Rimuovi</button>
+                            </li>
+                        </ul>
+                    </div>
+                    <span><strong>Totale:</strong>@{{ totalCart }}</span>
+                    <button class="btn btn-success">
+                        <a class="text-white text-decoration-none" v-on:click="setLocalStorage()"
+                            href="{{ route('checkout.create') }}">Procedi con il pagamento</a>
+                    </button>
+                </div>
+            </div>
+
+        </div>
         @endsection
         {{-- Added the script section for the Vue logic --}}
         @section('script-end')
-            <script>
-                Vue.config.devtools = true;
+        <script>
+            Vue.config.devtools = true;
                 // Initialized a new instance of 'vue'
                 const app = new Vue({
                     el: '#app',
@@ -143,5 +144,5 @@
 
 
                 })
-            </script>
+        </script>
         @endsection
