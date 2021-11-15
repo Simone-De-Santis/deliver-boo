1da3549622fd0600a998710ade66b38d971e9bc1
Giorgio Andrei <cipuandrei@libero.it>
2021-11-15 11:41:32 +0100

    store function defined

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -47,7 +47,44 @@ class PaymentController extends Controller
      */
     public function store(Request $request)
     {
-        //
+
+
+        $gateway = new Braintree\Gateway([
+            //^Li prendono dal file .env
+            'environment' => config('services.braintree.environment'),
+            'merchantId' => config('services.braintree.merchantId'),
+            'publicKey' => config('services.braintree.publicKey'),
+            'privateKey' => config('services.braintree.privateKey'),
+        ]);
+        /* Forse è con le quadre */
+        $amount = $request->amount;
+        $nonce = $request->payment_method_nonce;
+
+        $result = $gateway->transaction()->sale([
+            'amount' => $amount,
+            'paymentMethodNonce' => $nonce,
+            'options' => [
+                'submitForSettlement' => true
+            ]
+        ]);
+
+        if ($result->success) {
+            $transaction = $result->transaction;
+            /*  NON VA SCRITTO COSI MA COME SOTTO header("Location: " . $baseUrl . "transaction.php?id=" . $transaction->id); */
+            //^IN CASO DI TRANSAZIONE ANDATA A BUON FINE MOSTRO ANCHE L'ID
+            return back()->with('success_message', "Transaction successful. The ID is: $transaction->id");
+        } else {
+            $errorString = "";
+
+            foreach ($result->errors->deepAll() as $error) {
+                $errorString .= 'Error: ' . $error->code . ": " . $error->message . "\n";
+            }
+            /* 
+            $_SESSION["errors"] = $errorString;
+            header("Location: " . $baseUrl . "index.php"); */
+            //^IN CASO DI TRANSAZIONE ANDATA MALE
+            return back()->withErrors("An error occurred with the message: $result->message");
+        }
     }
 
     /**
