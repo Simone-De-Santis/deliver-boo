5bc44844032d8e08533f32e429f0552e0953616e
Simone-De-Santis <simone.agentelazio@yahoo.it>
2021-11-16 20:19:34 +0100

    add mod home guest

--- resources/views/Guest/home.blade.php
+++ resources/views/Guest/home.blade.php
@@ -23,87 +23,24 @@
         </option>
       </select>
     </nav>
-
-    <div class="container">
-      <div v-for="restaurant in selectedRestaurants" v-if="isVisible(restaurant.name)">
-        @{{ restaurant . activity_name }}
-        <div id="carouselExampleIndicators" class="carousel sz-slide" data-type="carousel" data-height="300px"
-          data-animation="dragX" data-width="100%">
-          {{-- <ol class="carousel-indicators">
-            <li data-target="#carouselExampleIndicators" class="active"></li>
-            <li data-target="#carouselExampleIndicators"></li>
-            <li data-target="#carouselExampleIndicators"></li>
-            <li data-target="#carouselExampleIndicators"></li>
-            <li data-target="#carouselExampleIndicators"></li>
-          </ol> --}}
-          {{-- <div class="carousel-inner">
-            <div class="carousel-item active">
-              <img src="/images/logo.jpg" class="d-block h-100 w-auto mx-auto" alt="">
+    <div class="container d-flex align-items-center flex-wrap">
+      <div class="row">
+        <div v-for="restaurant in selectedRestaurants" v-if="isVisible(restaurant.name)" class="col-md-4 col-sm-12 mb-3">
+          <div>
+            <div class="card bg-dark text-white">
+              <a :href="'/user/'+restaurant.id" class="text-white">
+                <img src="/images/fresh.jpg" alt="..." class="card-img" alt="...">
+                <div class="card-img-overlay">
+                  <h5 class="card-title">@{{ restaurant . activity_name }}</h5>
+                  <p class="card-text">inserire inizio descrizione</p>
+                  <p class="card-text">se vogliamo un footer</p>
+                </div>
+              </a>
             </div>
-
-
-            <div class="carousel-item ">
-              <img src="/images/Fast Delivery.jpg" class="d-block h-100 w-auto mx-auto" alt="">
-
-            </div>
-
-            <div class="carousel-item ">
-              <img src="/images/Urban.jpg" class="d-block h-100 w-auto mx-auto" alt="">
-
-            </div>
-            <div class="carousel-item ">
-              <img src="/images/Fast Delivery.jpg" class="d-block h-100 w-auto mx-auto" alt="">
-
-            </div>
-            <div class="carousel-item ">
-              <img src="/images/Urban.jpg" class="d-block h-100 w-auto mx-auto" alt="">
-
-            </div>
-
-
-          </div> --}}
-          <a class="carousel-control-prev carousel-control-btn" href="#carouselExampleIndicators" role="button">
-            <span class="carousel-control-prev-icon bg-dark" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-          </a>
-          <a class="carousel-control-next carousel-control-btn" href="#carouselExampleIndicators" role="button">
-            <span class="carousel-control-next-icon bg-dark" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-          </a>
-        </div>
-        {{-- <div class="carosel carousel slide" data-ride="carousel" id="carouselExampleIndicators">
-      <ul v-for="restaurant in restaurants" class=" d-flex carosel-two " style="transform:translateX(0px);">
-        <li class="slide  slide-two ">
-          <button class="short-cut-title" type="button">
-            <div class="" role="presentation">
-
-              <div class=""><span class="">Offerte</span></div>
-            </div>
-          </button>
-        </li>
-      </ul>
-      <a class="left carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-
-      </a>
-      <a class="right carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-
-      </a>
-    </div> --}}
-        <div class="card" style="width: 18rem;">
-          <img class="card-img-top" src="/images/fresh.jpg" alt="Card image cap">
-          <div class="card-body">
-            <h5 class="card-title">@{{ restaurant . activity_name }}</h5>
-            <p class="card-text">@{{ restaurant . description }}</p>
-            <h1>id @{{ restaurant . id }}</h1>
-            <a :href="'/user/'+restaurant.id" class="btn btn-warning">Dettagli</a>
           </div>
         </div>
-
       </div>
     </div>
-  </div>
   </div>
   </section>
 @endsection
