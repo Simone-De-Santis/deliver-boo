4db191c7bc9ab5d9d1988823c58dfebf78e3c7d7
Simone-De-Santis <simone.agentelazio@yahoo.it>
2021-11-17 11:30:12 +0100

    test 1

--- resources/views/Admin/home.blade.php
+++ resources/views/Admin/home.blade.php
@@ -1,13 +1,13 @@
-{{-- Added general layout  --}}
-@extends('layouts.app')
-{{--Added 'content' section to add page content  --}}
+{{-- Added general layout --}}
+@extends('layouts.app_admin')
+{{-- Added 'content' section to add page content --}}
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">          
-            <h1>Benvenuto {{ $user->name }} </h1>
-        </div>
-       <h2>
-           Statistiche di tutte cose
-       </h2>
+  <div class="container">
+    <div class="row justify-content-center">
+      <h1>Benvenuto {{ $user->name }} </h1>
     </div>
+    <h2>
+      Statistiche di tutte cose
+    </h2>
+  </div>
 @endsection
---
+++ resources/views/layouts/app_admin.blade.php
@@ -0,0 +1,108 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
+  <title>DeliverBoo</title>
+
+  <!-- Scripts -->
+  <script src="{{ asset('js/app.js') }}" defer></script>
+
+  <!-- Fonts -->
+  <link rel="dns-prefetch" href="//fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+  {{-- Fontawesome cdn --}}
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css"
+    integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <!-- Styles -->
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  @yield('sass')
+  @yield('cdns')
+  @yield('script-header')
+</head>
+
+<body>
+  <div id="app">
+    <div id="admin-nav">
+
+      <nav class="navbar navbar-expand-md">
+
+        <a class="navbar-brand" href="{{ url('/') }}">
+          <img class="logo" src="/images/Logo-nav.png" alt="">
+          <strong>DeliverBoo</strong>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <!-- Left Side Of Navbar -->
+          <ul class="navbar-nav mr-auto">
+          </ul>
+          <!-- Right Side Of Navbar -->
+          <ul class="navbar-nav ml-auto">
+            <!-- Authentication Links -->
+            @guest
+              <li class="nav-item">
+                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+              </li>
+              @if (Route::has('register'))
+                <li class="nav-item">
+                  <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                </li>
+              @endif
+            @else
+              <li class="nav-item dropdown">
+                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
+                  aria-haspopup="true" aria-expanded="false" v-pre>
+                  {{ Auth::user()->name }}
+                </a>
+                <div>
+                  {{-- Added item to the product list --}}
+                  {{-- </div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                        <a href="{{ route('admin.products') }}">My products</a>
+                                    </div> --}}
+                  <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="{{ route('admin.home') }}">
+                      Il mio ristorante
+                    </a>
+                    <a class="dropdown-item" href="{{ route('admin.products.index') }}">
+                      Prodotti
+                    </a>
+                    <a class="dropdown-item" href="{{ route('admin.ingredients.index') }}">
+                      Ingredienti
+                    </a>
+                    <a class="dropdown-item" href="{{ route('admin.orders.index') }}">
+                      Ordini
+                    </a>
+                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                                 document.getElementById('logout-form').submit();">
+                      {{ __('Logout') }}
+                    </a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                      @csrf
+                    </form>
+                  </div>
+              </li>
+            @endguest
+          </ul>
+        </div>
+
+      </nav>
+    </div>
+
+    <main class="py-4">
+      @yield('content')
+    </main>
+  </div>
+  @yield('script-end')
+</body>
+
+</html>
