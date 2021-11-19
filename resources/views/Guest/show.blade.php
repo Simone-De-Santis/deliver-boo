7049d6d057fdc2cbbe0c510992771f707cea8323
Roccofranchini <rocco.franchini1@gmail.com>
2021-11-19 10:48:15 +0100

    fix

--- resources/views/Guest/show.blade.php
+++ resources/views/Guest/show.blade.php
@@ -8,7 +8,7 @@
     {{-- Id app to add link with Vue --}}
     <div id="app">
         <section class="container my-5 pt-4" id="restaurant">
-            <h1 class="text-center my-3"> {{ $user->activity_name }} </h1>
+            <h1 class="text-center my-md-3"> {{ $user->activity_name }} </h1>
             <div id="products-section" class="row justify-content-center">
                 {{-- Products section --}}
                 <div class="col-12 col-md-6 d-flex flex-column align-items-center order-5 order-md-0">
@@ -38,7 +38,7 @@
                 </div>
                 {{-- Cart section --}}
                 <div class="col-12 col-md-6 d-flex justify-content-center order-1">
-                    <div class="w-100 h-100" v-if="cart.length > 0">
+                    <div class="w-100 h-100 d-flex justify-content-center" v-if="cart.length > 0">
                         <div class="card shadow-lg mb-5" id="cart-section" v-if="cart[0].user_id == {{ $user->id }}">
                             <div class="card-header primary-color-bg text-white">
                                 <h3 class="text-center my-1"><i class="fas fa-shopping-cart text-center"></i></h3>
