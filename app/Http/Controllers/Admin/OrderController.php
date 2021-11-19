312af97532c9f530e8375f38618351e842dd93df
Simone-De-Santis <simone.agentelazio@yahoo.it>
2021-11-19 12:28:28 +0100

    mod data order admin

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
+
 class OrderController extends Controller
 {
     /**
@@ -19,12 +20,12 @@ class OrderController extends Controller
     {
         $id = auth()->user()->id;
 
-        $user_orders = DB::select("SELECT orders.id FROM `orders`
-         INNER JOIN order_product
+        $user_orders = DB::select("SELECT `first_name`,`last_name`,`address`,`city`,`message_to_users`,`orders`.`created_at` FROM `orders`
+          INNER JOIN order_product
           ON orders.id = order_product.order_id 
           INNER JOIN products 
           ON products.id = order_product.product_id 
-          WHERE products.user_id = 1 
+          WHERE products.user_id = $id
           GROUP BY orders.id");
 
         /* $products = Order::with('products')->where('user_id', '=', $id)->get(); */
@@ -32,6 +33,7 @@ class OrderController extends Controller
         foreach ($products as $product) {
             $orderIds[] = $product->orders->pluck('id')->toArray();
         } */
+
         return view('admin.orders.index', compact('user_orders'));
     }
 
@@ -43,6 +45,21 @@ class OrderController extends Controller
      */
     public function show($id)
     {
+
+        // $id_users = auth()->user()->id;
+
+        // $user_orders = DB::select("SELECT `first_name`,`last_name`,`address`,`city`,`message_to_users`,`orders`.`created_at` FROM `orders`
+        //   INNER JOIN order_product
+        //   ON orders.id = order_product.order_id 
+        //   INNER JOIN products 
+        //   ON products.id = order_product.product_id 
+        //   WHERE products.user_id = $id_users
+        //   WHERE products.user_id = $id
+
+        //   GROUP BY orders.id");
+
+
+
         return view('admin.orders.show');
     }
 }
--- resources/views/Admin/orders/index.blade.php
+++ resources/views/Admin/orders/index.blade.php
@@ -3,21 +3,31 @@
 
 {{-- Added 'content' section to add page content --}}
 @section('content')
-    @dd($user_orders)
-    <h1>questa è la index dei ordini </h1>
-    <h3 class="mt-5">Ordini associati all'utente attuale:</h3>
-    <ul>
-        @foreach ($user_orders as $order)
-            <li>order id: {{ $order->id }}</li>
-            <li>order name: {{ $order->first_name }}
-                {{-- <h5 class="mt-4">Ingredienti:</h5> --}}
-                {{-- @foreach ($order->ingredients as $ingredient)
+
+  <h1>questa è la index dei ordini </h1>
+  <h3 class="mt-5">Ordini associati all'utente attuale:</h3>
+  <ul>
+    @foreach ($user_orders as $order)
+      {{-- <li>order id: {{ $order->id }}</li> --}}
+      <li>order name: {{ $order->first_name }}
+      <li>order address: {{ $order->address }}
+      <li>order city: {{ $order->city }}
+      <li>order message to users: {{ $order->message_to_users }}
+      <li>order ordine creato il :{{ $order->created_at }}
+
+        {{-- <h5 class="mt-4">Ingredienti:</h5> --}}
+        {{-- @foreach ($order->ingredients as $ingredient)
         <div>{{ $ingredient->id }}</div>
         <div>{{ $ingredient->name }}</div>
         <br>
     @endforeach --}}
-            </li>
-            <br>
-        @endforeach
-    </ul>
+      </li>
+      <br>
+    @endforeach
+  </ul>
+@endsection
+@section('script-end')
+  <script>
+
+  </script>
 @endsection
