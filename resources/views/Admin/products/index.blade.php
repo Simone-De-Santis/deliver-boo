94386e506db76616c732821b1de063df1291fd96
Rabab-art <bouhamla.rabab@gmail.com>
2021-11-21 14:57:21 +0100

    Modification products admin section

--- resources/views/Admin/products/index.blade.php
+++ resources/views/Admin/products/index.blade.php
@@ -3,56 +3,64 @@
 
 {{-- Added 'content' section to add page content --}}
 @section('content')
-  <section class="container">
-    <h1>Questa è la index dei prodotti </h1>
-    <h3 class="mt-5">Prodotti associati all'utente attuale:</h3>
-    <table class="table table-dark">
+<section class="container products-section ">
+
+  <h1 class="">I tuoi prodotti:</h1>
+  <div class="table-responsive">
+    <table class="table  order-table">
       <thead>
         <tr class="">
-          <th></th>
-          <th></th>
-          <th class=" text-right">
+         
+          <th class=" text-right" scope="col">
 
-            <a class="text-white text-decoration-none btn btn-primary mr-2"
+            <a class="text-white text-decoration-none btn mr-2" id="buttom"
               href="{{ route('admin.products.create') }}">Aggiungi un nuovo
               prodotto</a>
 
           </th>
         </tr>
       </thead>
+      <hr>
       <thead>
         <tr>
 
-          <th scope="col">Id</th>
-          <th scope="col">Name</th>
-          <th scope="col">Action</th>
+          <th scope="col">
+            <h4>Id</h4>
+          </th>
+          <th scope="col">
+            <h4>Tipo di prodotto</h4>
+          </th>
+          <th scope="col">
+            <h4>Attività</h4>
+          </th>
 
         </tr>
       </thead>
       <tbody>
         @foreach ($products as $product)
-          <tr>
-            <th>{{ $product->id }}</th>
-            <td>{{ $product->name }}</td>
-            <td>
-              <button type="button" class="btn btn-primary mr-2">
-                <a class="text-white text-decoration-none"
-                  href="{{ route('admin.products.show', $product->id) }}">Dettagli</a>
-              </button>
-              <button type="button" class="btn btn-success">
-                <a class="text-white text-decoration-none"
-                  href="{{ route('admin.products.edit', $product->id) }}">Modifica</a>
-              </button>
+        <tr scope="row"> 
+          <th>{{ $product->id }}</th>
+          <td>{{ $product->name }}</td>
+          <td>
+            <button type="button" class="btn btn-outline-secondary mr-2">
+              <a class="text-secondary text-decoration-none"
+                href="{{ route('admin.products.show', $product->id) }}">Dettagli</a>
+            </button>
+            <button type="button" class="btn btn btn-outline-success">
+              <a class="text-success text-decoration-none"
+                href="{{ route('admin.products.edit', $product->id) }}">Modifica</a>
+            </button>
 
-              <form action="{{ route('admin.products.destroy', $product->id) }}" method="post"
-                class="delete-button d-inline-block">
-                @csrf
-                @method('DELETE')
-                <button type="submit" class="btn btn-danger ml-2">Elimina</button>
-              </form>
-            </td>
-          </tr>
+            <form action="{{ route('admin.products.destroy', $product->id) }}" method="post"
+              class="delete-button d-inline-block">
+              @csrf
+              @method('DELETE')
+              <button type="submit" class="btn btn-outline-danger ml-2">Elimina</button>
+            </form>
+          </td>
+        </tr>
         @endforeach
     </table>
-  </section>
+  </div>
+</section>
 @endsection
