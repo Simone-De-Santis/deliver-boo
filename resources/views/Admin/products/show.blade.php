12db3105b6c29f00a762af9f70edcf5bdb27e74c
Simone-De-Santis <simone.agentelazio@yahoo.it>
2021-11-21 22:19:11 +0100

    add image to sho admin product

--- resources/views/Admin/products/show.blade.php
+++ resources/views/Admin/products/show.blade.php
@@ -7,9 +7,13 @@
     <h1>Questa è la show del prodotto {{ $product->id }} </h1>
 
     <div class="card mt-4" style="width: 18rem;">
-      <img class="card-img-top"
-        src="{{ $product->url ? 'http://127.0.0.1:8000/storage/$product->url' : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9R8JNXHezmElwOman5hc0r6fBmIOgZtDgCw&usqp=CAU' }}" " alt="
-        immagine-prodotto">
+      <img class="card-img-top" @if ($product->url)
+      src="{{ asset('storage/' . $product->url) }}"
+
+    @else
+      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9R8JNXHezmElwOman5hc0r6fBmIOgZtDgCw&usqp=CAU" @endif
+      alt="
+      immagine-prodotto">
       <div class="card-body">
         <h5 class="card-title">{{ $product->name }}</h5>
         <p class="card-text">{{ $product->description }}</p>
