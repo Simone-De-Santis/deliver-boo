ed2124ffaadd8ed257f71eac5a46422bc70aedc7
Rabab-art <bouhamla.rabab@gmail.com>
2021-11-21 16:35:26 +0100

    Resposive product admin section

--- resources/views/Admin/products/index.blade.php
+++ resources/views/Admin/products/index.blade.php
@@ -6,18 +6,23 @@
 <section class="container products-section ">
 
   <h1 class="">I tuoi prodotti:</h1>
-  <div class="table-responsive">
+  
     <table class="table  order-table">
       <thead>
         <tr class="">
          
-          <th class=" text-right" scope="col">
+          <th></th>
+          <th></th>
+          <th class=" text-left" scope="col">
+            
 
             <a class="text-white text-decoration-none btn mr-2" id="buttom"
               href="{{ route('admin.products.create') }}">Aggiungi un nuovo
               prodotto</a>
 
           </th>
+          
+           
         </tr>
       </thead>
       <hr>
@@ -38,29 +43,29 @@
       </thead>
       <tbody>
         @foreach ($products as $product)
-        <tr scope="row"> 
-          <th>{{ $product->id }}</th>
-          <td>{{ $product->name }}</td>
-          <td>
-            <button type="button" class="btn btn-outline-secondary mr-2">
+        <tr scope="row">
+          <th scope="col" class="info-product">{{ $product->id }}</th>
+          <td scope="col" class="info-product">{{ $product->name }}</td>
+          <td scope="col">
+            <button type="button" class="btn btn-outline-secondary mr-2 mt-3">
               <a class="text-secondary text-decoration-none"
                 href="{{ route('admin.products.show', $product->id) }}">Dettagli</a>
             </button>
-            <button type="button" class="btn btn btn-outline-success">
+
+            <button type="button" class="btn btn btn-outline-success mr-2 mt-3">
               <a class="text-success text-decoration-none"
                 href="{{ route('admin.products.edit', $product->id) }}">Modifica</a>
-            </button>
 
-            <form action="{{ route('admin.products.destroy', $product->id) }}" method="post"
-              class="delete-button d-inline-block">
-              @csrf
-              @method('DELETE')
-              <button type="submit" class="btn btn-outline-danger ml-2">Elimina</button>
-            </form>
+              <form action="{{ route('admin.products.destroy', $product->id) }}" method="post"
+                class="delete-button d-inline-block  ">
+                @csrf
+                @method('DELETE')
+                <button type="submit" class="btn btn-outline-danger mt-3 ">Elimina</button>
+              </form>
           </td>
         </tr>
         @endforeach
     </table>
-  </div>
+
 </section>
 @endsection
