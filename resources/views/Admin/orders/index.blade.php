91a28861ac7bb18031ac6227a7ee852c1658b7e9
Giorgio Andrei <cipuandrei@libero.it>
2021-11-22 12:31:43 +0100

    -

--- resources/views/Admin/orders/index.blade.php
+++ resources/views/Admin/orders/index.blade.php
@@ -4,46 +4,46 @@
 {{-- Added 'content' section to add page content --}}
 @section('content')
 
-<section class="container">
-  
-  <h2 class="mt-5 text-white">Ordini effetuati:</h2>
+    <section class="container">
 
-  <div class="card order-card" >
-    <div class="card-body p-3 container-fluid">
-      
-      <ul class="orders-info  ">
-        @foreach ($user_orders as $order) 
-          {{-- <li>order id: {{ $order->id }}</li> --}}
-          <li>Nome e Cognome: {{ $order->first_name }}  {{ $order->last_name }}</li>
-          <hr>
-          <li>Indirizzo: {{ $order->address }}</li>
-          <hr>
-          <li>Città: {{ $order->city }}</li>
-          <hr>
-          <li>Data ordine : {{ $order->created_at }}</li>
-          <hr>
-          <li style="font-size: 18px">Totale: € {{ $order->total_price }} </li>
-          <hr>
-          <button type="button" class="btn btn-dark mb-4"><a class="text-white text-decoration-none"
-              href="{{ route('admin.orders.show', $order->id) }}"> Dettagli</a></button>
-    
-         
-        @endforeach
-      </ul>
-<br>
-      <br>
+        <h2 class="mt-5 text-white">Ordini effetuati:</h2>
+
+        <div class="card order-card">
+            <div class="card-body p-3 container-fluid">
+
+                <ul class="orders-info  ">
+                    @foreach ($user_orders as $order)
+                        {{-- <li>order id: {{ $order->id }}</li> --}}
+                        <li>Nome e Cognome: {{ $order->first_name }} {{ $order->last_name }}</li>
+                        <hr>
+                        <li>Indirizzo: {{ $order->address }}</li>
+                        <hr>
+                        <li>Città: {{ $order->city }}</li>
+                        <hr>
+                        <li>Data ordine : {{ $order->created_at }}</li>
+                        <hr>
+                        <li style="font-size: 18px">Totale: €{{ $order->total_price }} </li>
+                        <hr>
+                        <button type="button" class="btn btn-dark mb-4"><a class="text-white text-decoration-none"
+                                href="{{ route('admin.orders.show', $order->id) }}"> Dettagli</a></button>
 
 
-    </div>
-  </div>
+                    @endforeach
+                </ul>
+                <br>
+                <br>
 
 
- 
+            </div>
+        </div>
 
-</section>
+
+
+
+    </section>
 @endsection
 @section('script-end')
-  <script>
+    <script>
 
-  </script>
+    </script>
 @endsection
